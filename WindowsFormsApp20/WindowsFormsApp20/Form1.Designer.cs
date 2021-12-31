1cac665e81c41f1f7f7c8b917c12ff56e930b0df
ali zedan <92272920+AliZedan1A@users.noreply.github.com>
2021-12-31 15:22:57 +0200

    Add files via upload

---
+++ WindowsFormsApp20/WindowsFormsApp20/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace WindowsFormsApp20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 79);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(310, 37);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(78, 130);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "تشفير";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 215);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "eng",
+            "numbers"});
+            this.comboBox1.Location = new System.Drawing.Point(182, 138);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(206, 37);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(46, 256);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "نسخ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(481, 307);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Traditional Arabic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "تشفير";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp20/WindowsFormsApp20/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] Letters = {
+              // 1
+    "a",          // 2
+    "b",          // 3
+    "c",          // 4
+    "d",          // 5
+    "e",          // 6
+    "f",          // 7
+    "g",          // 8
+    "h",          // 9
+    "i",          // 10
+    "j",          // 11
+    "k",          // 12
+    "l",          // 13
+    "m",          // 14
+    "n",          // 15
+    "o",          // 16
+    "p",          // 17
+    "q",          // 18
+    "r",          // 19
+    "s",          // 20
+    "t",          // 21
+    "u",          // 22
+    "v",          // 23
+    "w",          // 24
+    "x",          // 25
+    "y",          // 26
+    "z",          // 27
+            };
+            string[] Mores = {
+ "[01]",       // 2
+    "[1000]",     // 3
+    "[1010]",     // 4
+    "[100]",      // 5
+    "[0]",        // 6
+    "[0010]",     // 7
+    "[110]",      // 8
+    "[0000]",     // 9
+    "[00]",       // 10
+    "[0111]",     // 11
+    "[101]",      // 12
+    "[0100]",     // 13
+    "[11]",       // 14
+    "[10]",       // 15
+    "[111]",      // 16
+    "[0110]",     // 17
+    "[1101]",     // 18
+    "[010]",      // 19
+    "[000]",      // 20
+    "[1]",        // 21
+    "[001]",      // 22
+    "[0001]",     // 23
+    "[011]",      // 24
+    "[1001]",     // 25
+    "[1011]",     // 26
+    "[1100]",         };
+
+
+            if (comboBox1.Text =="eng")
+            {
+                for (var i = 0; i < 26; i++)
+                {
+                    textBox1.Text = textBox1.Text.Replace(Letters[i], Mores[i]);
+                }
+                label1.Text = textBox1.Text;
+                textBox1.Text = string.Empty;
+            }
+            else if(comboBox1.Text== "numbers")
+            {
+                for (var i = 0; i < 26; i++)
+                {
+                    textBox1.Text = textBox1.Text.Replace(Mores[i], Letters[i]);
+                }
+                label1.Text = textBox1.Text;
+                textBox1.Text = string.Empty;
+            }
+            else if(textBox1.Text ==string.Empty)
+            {
+                MessageBox.Show("عليك وضع شيء لتشفيره", "تنبيه", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(" يجب عليك ملأ الحقول ", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Warning) ;
+            }
+            
+
+
+
+           
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(label1.Text==string.Empty)
+            {
+                MessageBox.Show("لا يوجد شيء لنسخه", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Clipboard.SetText(label1.Text);
+                MessageBox.Show(label1.Text+" : لقد تم  نسخ النص");
+
+            }
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            if(comboBox1.Text =="numbers")
+            {
+                MessageBox.Show( "[الرقم]"+ " : عليك وضع الارقام داخل قوسان", "تنبيه", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                MessageBox.Show( "الرجاء ملئ الحقول لكي يعمل البرنامج بشكل سليم", "تنبيه", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+    }
+}
